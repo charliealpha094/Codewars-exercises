98fb6c7bd8820ba7b4b73806f705b651264129d5
charliealpha094 <carlosamaral94@gmail.com>
2021-09-01 12:39:19 +0100

    initial commit

---
+++ fundamentals/returnStrings.js
@@ -0,0 +1,6 @@
+// Done by Carlos Amaral (2021/09/01)
+
+/*
+Make a function that will return a greeting statement that uses an input; 
+your program should return, "Hello, <name> how are you doing today?".
+*/
