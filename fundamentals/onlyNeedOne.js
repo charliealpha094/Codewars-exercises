139d5e1d9f80da707c6b0c3d16699640ab206f52
charliealpha094 <carlosamaral94@gmail.com>
2021-09-02 15:24:41 +0100

    initial commit

---
+++ fundamentals/onlyNeedOne.js
@@ -0,0 +1,18 @@
+// Done by Carlos Amaral (2021/09/02)
+
+/*
+You will be given an array "a" and a value x. All you need to do is check whether
+ the provided array contains the value.
+
+Array can contain numbers or strings. X can be either.
+
+Return true if the array contains the value, false if not.
+*/
+
+function check(a, x) {
+  if (a.includes(x)) {
+    return true;
+  } else {
+    return false;
+  }
+}
