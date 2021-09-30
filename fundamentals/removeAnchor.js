0f42255ad52d7ebf22abb142a03cfe9a7cabd6f6
charliealpha094 <carlosamaral94@gmail.com>
2021-10-01 00:03:09 +0100

    initial commit

---
+++ fundamentals/removeAnchor.js
@@ -0,0 +1,19 @@
+// Done by Carlos Amaral (2021/10/01)
+
+/*
+Complete the function/method so that it returns the url with anything after the
+anchor (#) removed.
+
+Examples
+// returns 'www.codewars.com'
+removeUrlAnchor('www.codewars.com#about')
+
+// returns 'www.codewars.com?page=1' 
+removeUrlAnchor('www.codewars.com?page=1') 
+*/
+
+function removeUrlAnchor(url) {
+  return url.split("#")[0];
+}
+
+console.log(removeUrlAnchor("www.codewars.com#about"));
