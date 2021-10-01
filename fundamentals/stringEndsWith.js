2a19161812be00ff21a5ae74702b55131eba64f0
charliealpha094 <carlosamaral94@gmail.com>
2021-10-02 00:21:50 +0100

    initial commit

---
+++ fundamentals/stringEndsWith.js
@@ -0,0 +1,17 @@
+// Done by Carlos Amaral (2021/10/01)
+
+/* 
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+Examples:
+
+solution('abc', 'bc') // returns true
+solution('abc', 'd') // returns false
+*/
+
+function solution(str, ending) {
+  return str.endsWith(ending);
+}
+
+console.log(solution("abc", "bc"));
+console.log(solution("abc", "d"));
