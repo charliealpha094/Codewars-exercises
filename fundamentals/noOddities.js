cbbcd9cb168ffe49e7b2765640f3a0e9db4de9b5
charliealpha094 <carlosamaral94@gmail.com>
2021-10-21 18:53:09 +0100

    initial commit

---
+++ fundamentals/noOddities.js
@@ -0,0 +1,14 @@
+// Done by Carlos Amaral (2021/10/21)
+
+/* 
+Write a small function that returns the values of an array that are not odd.
+
+All values in the array will be integers. Return the good values in the order 
+they are given.
+*/
+
+function noOdds(values) {
+    return values.filter(function) {
+        return n % 2 === 0;
+    }
+}
