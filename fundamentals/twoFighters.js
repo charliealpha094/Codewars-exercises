9f7eb94721d1e308e0e98dbe34a267092b13e75c
charliealpha094 <carlosamaral94@gmail.com>
2021-10-26 11:27:54 +0100

    initial commit

---
+++ fundamentals/twoFighters.js
@@ -0,0 +1,29 @@
+// Done by Carlos Amaral (2021/10/26)
+
+/* 
+Create a function that returns the name of the winner in a fight between two fighters.
+Each fighter takes turns attacking the other and whoever kills the other first is 
+victorious. Death is defined as having health <= 0.
+
+Each fighter will be a Fighter object/instance. See the Fighter class below in
+ your chosen language.
+Both health and damagePerAttack (damage_per_attack for python) will be integers
+ larger than 0. You can mutate the Fighter objects. 
+ 
+ Example:
+function Fighter(name, health, damagePerAttack) {
+        this.name = name;
+        this.health = health;
+        this.damagePerAttack = damagePerAttack;
+        this.toString = function() { return this.name; }
+}*/
+
+function declareWinner(fighter1, fighter2, firstAttacker) {
+  while (fighter1.health >= 0 && fighter2.health >= 0) {
+    fighter1.health -= fighter2.damagePerAtack;
+    fighter2.health -= fighter1.damagePerAtack;
+  }
+  if (fighter1.health <= 0 && fighter2.health <= 0) return firstAttacker;
+  else if (fighter1.health <= 0) return fighter2.name;
+  else return fighter2.name;
+}
