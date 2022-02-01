ed8f09bd6c6ecaa6202a11e6152e15edb3f662b9
charliealpha094 <carlosamaral94@gmail.com>
2022-02-01 11:51:29 +0000

    initial commit

---
+++ fundamentals/lostWithoutMap.js
@@ -0,0 +1,13 @@
+// Done by Carlos Amaral (2022/02/01)
+
+/*
+Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6]
+*/
+
+function maps(x) {
+  return x.map((n) => n * 2);
+}
