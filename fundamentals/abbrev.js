cb4b53e4efed907044d782dd7282a4814aca79f0
charliealpha094 <carlosamaral94@gmail.com>
2022-02-01 11:24:38 +0000

    initial commit

---
+++ fundamentals/abbrev.js
@@ -0,0 +1,23 @@
+// Done by Carlos Amaral (2022/02/01)
+
+/*
+Write a function to convert a name into initials. This kata strictly takes two 
+words with one space in between them.
+The output should be two capital letters with a dot separating them.
+
+It should look like this:
+
+Sam Harris => S.H
+
+patrick feeney => P.F
+*/
+
+function abbrevName(name) {
+  return name
+    .toUpperCase()
+    .split(" ")
+    .map((name) => name[0])
+    .join(".");
+}
+
+console.log(abbrevName("Carlos Amaral"));
