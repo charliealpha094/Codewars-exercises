d9ccbd35212acb39c8c9799a17f0879085af0f78
charliealpha094 <carlosamaral94@gmail.com>
2022-02-11 18:49:51 +0000

    initial commit

---
+++ fundamentals/reversedSequence.js
@@ -0,0 +1,15 @@
+// Done by Carlos Amaral (2022/02/11)
+
+/*
+Build a function that returns an array of integers from n to 1 where n>0.
+
+Example : n=5 --> [5,4,3,2,1]
+*/
+
+const reverseSeq = (n) => {
+  let arr = [];
+  for (let i = n; i > 0; i--) {
+    arr.push(i);
+  }
+  return arr;
+};
