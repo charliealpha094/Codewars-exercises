d22d91f313dd5fddd099f97eb84de94f21ca7887
charliealpha094 <carlosamaral94@gmail.com>
2022-02-17 11:24:09 +0000

    initial commit

---
+++ fundamentals/superDuperEasy.js
@@ -0,0 +1,10 @@
+// Done by Carlos Amaral (2022/02/16)
+
+/*
+Make a function that returns the value multiplied by 50 and increased by 6. If 
+the value entered is a string it should return "Error".
+*/
+
+const problem = function () {
+  return typeof x === "string" ? "Error" : x * 50 + 6;
+};
