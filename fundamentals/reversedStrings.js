33c6f65ac64cea8d938d652c52c619d0bd18879e
charliealpha094 <carlosamaral94@gmail.com>
2022-02-18 00:32:51 +0000

    initial commit

---
+++ fundamentals/reversedStrings.js
@@ -0,0 +1,12 @@
+// Done by Carlos Amaral (2022/02/18)
+
+/*
+Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+*/
+
+const solution = function (str) {
+  return str.split().reverse().join("");
+};
