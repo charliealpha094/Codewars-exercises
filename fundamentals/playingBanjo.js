8af40ea60048ac52cfcf122ea53c4d8e1401e972
charliealpha094 <carlosamaral94@gmail.com>
2022-02-24 00:24:41 +0000

    initial commit

---
+++ fundamentals/playingBanjo.js
@@ -0,0 +1,21 @@
+// Done by Carlos Amaral (2022/02/24)
+
+/*
+Create a function which answers the question "Are you playing banjo?".
+If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+The function takes a name as its only argument, and returns one of the following
+strings:
+
+name + " plays banjo" 
+name + " does not play banjo"
+Names given are always valid strings.
+*/
+
+const areYouPlayingBanjo = function (name) {
+  if (name[0] === "R" || name[0] === "r") {
+    return name + " plays banjo";
+  } else {
+    return name + " does not play banjo";
+  }
+};
