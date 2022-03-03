b7e1a01cb5ec44fccb8380e24790950969b9755b
charliealpha094 <carlosamaral94@gmail.com>
2022-03-03 01:22:04 +0000

    initial commit

---
+++ fundamentals/hexToDecimal.js
@@ -0,0 +1,10 @@
+// Done by Carlos Amaral (2022/03/03)
+
+/*
+Complete the function which converts hex number (given as a string) to a 
+decimal number.
+*/
+
+function hexToDec(hexString) {
+  return parseInt(hexString, 16);
+}
