9fa3f52679ad6252172f66a34fc32bfa862473a8
charliealpha094 <carlosamaral94@gmail.com>
2022-03-09 19:10:43 +0000

    initial commit

---
+++ fundamentals/gameCombat.js
@@ -0,0 +1,9 @@
+// Done by Carlos Amaral (2022/03/09)
+
+/*
+Create a combat function that takes the player's current health and the amount
+ of damage recieved, and returns the player's new health. Health can't 
+ be less than 0.
+*/
+
+const combat = function () {};
