84504e91fb770bbeb6cef622db6ecad0321cc0b6
charliealpha094 <carlosamaral94@gmail.com>
2022-03-12 00:22:40 +0000

    initial commit

---
+++ fundamentals/repeater.js
@@ -0,0 +1,14 @@
+// Done by Carlos Amaral (2022/03/12)
+
+/*
+Write a function named repeater() that takes two arguments (a string and a 
+number), and returns a new string where the input string is repeated that
+ many times.
+
+Example: (Input1, Input2 --> Output)
+"a", 5 --> "aaaaa"
+*/
+
+const repeater = function (string, n) {
+  return string.repeat(n);
+};
